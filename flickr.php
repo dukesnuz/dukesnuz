5a5a850b9758c5c834d60632c5f51d883723b624
David Petringa <david@ajaxtransport.com>
2015-10-26 08:23:35 -0500

    Created flickr page

---
+++ flickr.php
@@ -0,0 +1,16 @@
+<?php
+/***************************This is the flickr page for dukesnuz******************************/
+/******I have split the site pages up into sections*****************************************/
+/*******Header, main section, footer*******************************************************/
+/****This also uses pages flickr.inc.html, flickr.php, flickr.js***************************/
+
+$title = "flickr";
+
+include('./views/header.inc.html');
+
+include('./views/flickr.inc.html');
+
+include('./views/footer.inc.html');
+
+?>
+     
