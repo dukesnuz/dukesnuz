7b6ddc8759cd9d86d465a7abf1f638b4f2303626
David Petringa <david@ajaxtransport.com>
2015-12-07 07:53:31 -0600

    A CSS3 Radial Gradient & Transform

---
+++ coding-playground/playground_12_04_2015.php
@@ -0,0 +1,17 @@
+<?php
+/***************************This is an example of linear radaial gradient for dukesnuz******************************/
+/**********   playground_12_04_2015.php  ***************************************************************************/
+/******I have split the site pages up into sections*****************************************/
+/*******Header, main section, footer*******************************************************/
+/****This also uses page playground_12_04_2015.inc.html***************************/
+
+$title = "css3 radial gradient transform rotate scale";
+
+include('../views/header.inc.html');
+
+include('../views/playground_12_04_2015.inc.html');
+
+include('../views/footer.inc.html');
+
+?>
+     
