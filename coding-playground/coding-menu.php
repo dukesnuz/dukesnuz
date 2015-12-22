82c3fdde6ca0573ba089edc3744bb28e7fb77561
David Petringa <david@ajaxtransport.com>
2015-12-22 14:42:34 -0600

    Page created to display self studies
    Created page to display self studies taken with links to store page and
    self study home pages

---
+++ coding-playground/coding-menu.php
@@ -0,0 +1,17 @@
+<?php
+/***************************This is the main menu with alist of all my coding tutorials******************************/
+/********** playground-menu.php  ***************************************************************************/
+/******I have split the site pages up into sections*****************************************/
+/*******Header, main section, footer*******************************************************/
+/****This also uses page coding_playground.inc.html***************************/
+
+$title = "coding playground | menu";
+
+include('../views/header.inc.html');
+
+include('../views/coding_playground.inc.html');
+
+include('../views/footer.inc.html');
+
+?>
+     
