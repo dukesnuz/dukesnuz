07170aef14e8996506637ecc93d3c29b15773e92
David Petringa <david@ajaxtransport.com>
2015-12-22 14:55:57 -0600

    Renamed file
    Renamed file name so that it can be used in new rewrite rule that uses
    file name in url

---
+++ self-study-courses/menu.php
@@ -0,0 +1,17 @@
+<?php
+/***************************This is the main menu with alist of all my coding tutorials******************************/
+/********** menu.php  ***************************************************************************/
+/******I have split the site pages up into sections*****************************************/
+/*******Header, main section, footer*******************************************************/
+/****This also uses page self_study_courses.inc.html***************************/
+
+$title = "self study courses | menu";
+
+include('../views/header.inc.html');
+
+//include('../views/self_study_courses.inc.html');
+
+include('../views/footer.inc.html');
+
+?>
+     
