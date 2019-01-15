73834e1d1d458898d55c996d40cbf6b97d96d61b
David Petringa <david@ajaxtransport.com>
2019-01-15 16:01:12 -0500

    first commmit for vue_crash_todolist

---
+++ self-study-courses/vuejs/vue_crash_todolist/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <Header />
+    <router-view/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+
+export default {
+  name:"app",
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: boeder-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
+}
+</style>
---
+++ self-study-courses/vuejs/vue_crash_todolist/src/components/layout/Footer.vue
@@ -0,0 +1,51 @@
+
+<template>
+  <footer class="footer">
+    <ul>
+      <li>
+        An AWESOME tutorial for Vue.js called
+        <a
+          href="https://www.youtube.com/watch?v=Wy9q22isx3U"
+          target="blank"
+        >Vue JS Crash Course - 2019</a>
+      </li>
+      <li>
+        Instructor:
+        <a href="http://www.traversymedia.com" target="blank">Traversy Media</a>
+      </li>
+      <li>
+        Student:
+        <a href="http://www.dukesnuz.com" target="blank">David Petringa</a>
+      </li>
+      <li>I completed this tutorial - January 2019</li>
+    </ul>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style scoped>
+.footer {
+  margin-top: 250px;
+  background: #ccc;
+  color: #333;
+  padding: 10px 0 250px 25px;
+}
+
+p {
+  margin-left: 10px;
+}
+
+a {
+  text-decoration: none;
+}
+
+ul {
+  list-style-type: none;
+}
+</style>
+ 
