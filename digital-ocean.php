3237b9fdda9036af72b9f3644d15def7a6da31b5
David Petringa <david@davidpetringa.com>
2022-02-23 16:20:40 -0600

    created referral link page for digital ocean

--- digital-ocean.php
+++ digital-ocean.php
@@ -3,7 +3,7 @@
 /*************************************************************************************************************
 *********************************this page created to display a referel  link to digital ocean***************/
 $title = "Digital Ocean";
-include('./views/header.inc.html');
+//include('./views/header.inc.html');
 ?>
 <?php include('views/digital_ocean.inc.html'); ?>
 <?php include('./views/footer.inc.html'); ?>
